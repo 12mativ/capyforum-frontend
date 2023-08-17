cc23a8d65714dc2478b24adee87aa14f612d647b
12mativ <ivan_m146@mail.ru>
2023-08-18 00:02:46 +0400

    Fixed types for session in useProfile

--- hooks/useProfile.tsx
+++ hooks/useProfile.tsx
@@ -1,59 +1,60 @@
-import { useEffect, useState, createContext, useContext } from 'react';
+import {useEffect, useState, createContext, useContext} from 'react'
 import {
   useUser as useSupaUser,
   useSessionContext,
   User,
-  Session
-} from '@supabase/auth-helpers-react';
+  Session,
+} from '@supabase/auth-helpers-react'
 
-import { ProfileDetails } from '@/types';
+import {ProfileDetails} from '@/types'
 
 type ProfileContextType = {
-  session: Session
-  accessToken: string | null;
-  profile: User | null;
-  profileDetails: ProfileDetails | null;
-  isLoading: boolean;
-};
+  session: Session | null
+  accessToken: string | null
+  profile: User | null
+  profileDetails: ProfileDetails | null
+  isLoading: boolean
+}
 
 export const ProfileContext = createContext<ProfileContextType | undefined>(
   undefined
-);
+)
 
 export interface Props {
-  [propName: string]: any;
+  [propName: string]: any
 }
 
 export const MyProfileContextProvider = (props: Props) => {
   const {
     session,
     isLoading: isLoadingUser,
-    supabaseClient: supabase
-  } = useSessionContext();
-  const profile = useSupaUser();
-  const accessToken = session?.access_token ?? null;
-  const [isLoadingData, setIsloadingData] = useState(false);
-  const [profileDetails, setProfileDetails] = useState<ProfileDetails | null>(null);
+    supabaseClient: supabase,
+  } = useSessionContext()
+  const profile = useSupaUser()
+  const accessToken = session?.access_token ?? null
+  const [isLoadingData, setIsloadingData] = useState(false)
+  const [profileDetails, setProfileDetails] = useState<ProfileDetails | null>(
+    null
+  )
 
-  const getUserDetails = async () => await supabase.from('profiles').select('*').single();
+  const getUserDetails = async () =>
+    await supabase.from('profiles').select('*').single()
 
   useEffect(() => {
     if (profile && !isLoadingData && !profileDetails) {
-      setIsloadingData(true);
-      Promise.allSettled([getUserDetails()]).then(
-        (results) => {
-          const profileDetailsPromise = results[0];
+      setIsloadingData(true)
+      Promise.allSettled([getUserDetails()]).then((results) => {
+        const profileDetailsPromise = results[0]
 
-          if (profileDetailsPromise.status === 'fulfilled')
-            setProfileDetails(profileDetailsPromise.value.data as ProfileDetails);
+        if (profileDetailsPromise.status === 'fulfilled')
+          setProfileDetails(profileDetailsPromise.value.data as ProfileDetails)
 
-          setIsloadingData(false);
-        }
-      );
+        setIsloadingData(false)
+      })
     } else if (!profile && !isLoadingUser && !isLoadingData) {
-      setProfileDetails(null);
+      setProfileDetails(null)
     }
-  }, [profile, isLoadingUser]);
+  }, [profile, isLoadingUser])
 
   const value = {
     session,
@@ -61,15 +62,15 @@ export const MyProfileContextProvider = (props: Props) => {
     profile,
     profileDetails,
     isLoading: isLoadingUser || isLoadingData,
-  };
+  }
 
   return <ProfileContext.Provider value={value} {...props} />
-};
+}
 
 export const useProfile = () => {
-  const context = useContext(ProfileContext);
+  const context = useContext(ProfileContext)
   if (context === undefined) {
-    throw new Error(`useUser must be used within a MyProfileContextProvider.`);
+    throw new Error(`useUser must be used within a MyProfileContextProvider.`)
   }
-  return context;
-};
+  return context
+}
